170a155731bcb4915ed0cc9045e894dcc47f6bc4
silvy8a <157405334+silvy8a@users.noreply.github.com>
2024-05-18 10:54:02 +0000

    Commit 7

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 
 #Loading the model
-model_path = "/models/tree_classifier_crit-gini_maxdepth-5_minleaf-1_minsplit-2_8.sav"
+model_path = "/workspaces/machine-learning-streamlit/models/tree_classifier_crit-gini_maxdepth-5_minleaf-1_minsplit-2_8.sav"
 
 try:
     with open(model_path, "rb") as model_file:
